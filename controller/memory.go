1bc617f53b3231219d440df9d18fe6a2f8bd06a7
Elvin <vinzln1209@gmail.com>
2022-12-21 21:50:06 +0700

    ambil bytes melalui path selesai, ELVIN

--- controller/memory.go
+++ controller/memory.go
@@ -2,8 +2,12 @@ package controller
 
 import (
 	"Project_BNCC_GO/model"
+	"bytes"
 	"fmt"
+	"image"
+	"image/png"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -13,18 +17,65 @@ import (
 var db *gorm.DB
 
 func CreateMemory(c echo.Context) error {
-	m := new(model.Memory)
 
-	if err := c.Bind(&m); err != nil {
-		panic(err)
+	webData := struct {
+		Memoryid     uint        `json:"memoryid,omitempty"`
+		DateAdded    time.Time   `json:"dateAdded"`
+		DateModified time.Time   `json:"dateModified"`
+		Desc         string      `json:"MemoryDesc"`
+		UserId       uint        `json:"UserId"`
+		PictureId    uint        `json:"pictureId,omitempty"`
+		Path         []string    `json:"PicturePath"`
+		Tag          []model.Tag `json:"tags"`
+	}{}
+
+	if err := c.Bind(&webData); err != nil {
+		panic("Error di binding data")
 	}
-	m.DateModified = time.Now()
-	fmt.Println(m)
-	result := db.Create(m)
+
+	picturesbyte := [][]byte{}
+
+	for _, value := range webData.Path {
+		imagefile, err := os.Open(value)
+		if err != nil {
+			panic("path invalid")
+		}
+		imageData, _, err := image.Decode(imagefile)
+		buff := new(bytes.Buffer)
+		err = png.Encode(buff, imageData)
+
+		if err != nil {
+			panic(err)
+		}
+
+		picturesbyte = append(picturesbyte, buff.Bytes())
+	}
+
+	pictures := []model.Picture{}
+
+	for _, value := range picturesbyte {
+		pic := model.Picture{
+			Picture: value,
+		}
+
+		pictures = append(pictures, pic)
+
+	}
+
+	memory := model.Memory{
+		Memoryid:     webData.Memoryid,
+		DateAdded:    webData.DateAdded,
+		DateModified: webData.DateModified,
+		Desc:         webData.Desc,
+		UserId:       webData.UserId,
+		Tag:          webData.Tag,
+		Picture:      pictures,
+	}
+	result := db.Create(&memory)
 
 	fmt.Println(result)
 
-	return c.Redirect(http.StatusOK, "localhost:5555")
+	return c.JSON(http.StatusOK, "Insert Successful")
 }
 
 func init() {
--- model/memory.go
+++ model/memory.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type Memory struct {
-	Memoryid     uint      `gorm:"column:memoryid;primarykey;auto_increment" json:"memoryid,omitempty"`
-	DateAdded    time.Time `gorm:"column:dateAdded;" json:"dateAdded"`
-	DateModified time.Time `gorm:"column:dateModified;" json:"dateModified"`
-	Desc         string    `gorm:"column:MemoryDesc;" json:"MemoryDesc"`
-	UserId       uint      `gorm:"column:userId" json:"UserId"`
-	Picture      []Picture `gorm:"foreignkey:MemoryId;references:Memoryid" json:"pictures"`
-	Tag          []Tag     `gorm:"foreignKey:MemoryId;referencecs:Memoryid" json:"tags"`
+	Memoryid     uint      `gorm:"column:memoryid;primarykey;auto_increment"`
+	DateAdded    time.Time `gorm:"column:dateAdded;"`
+	DateModified time.Time `gorm:"column:dateModified;"`
+	Desc         string    `gorm:"column:MemoryDesc;"`
+	UserId       uint      `gorm:"column:userId"`
+	Picture      []Picture `gorm:"foreignkey:MemoryId;references:Memoryid"`
+	Tag          []Tag     `gorm:"foreignKey:MemoryId;referencecs:Memoryid"`
 }
