f2fab7457db9ded27fa496271ca54c9fdb375baa
Alviannn <alvianndq@gmail.com>
2023-01-03 23:23:22 +0700

    fix(user): fix cannot create models because `email` is unsupported for using index

--- model/user.go
+++ model/user.go
@@ -3,7 +3,9 @@ package model
 type User struct {
 	BaseModel
 	Username string
-	Email     string    `gorm:"column:email;uniqueIndex;"`
+	// by default, `string` uses `longtext` type but MySQL doesn't support "foreign key" or "index"
+	// on that data type. source: https://stackoverflow.com/a/69201429
+	Email    string `gorm:"uniqueIndex;type:varchar(255)"`
 	Password string
 	Memories []Memory
 }
