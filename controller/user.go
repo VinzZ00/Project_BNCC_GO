3912d21696bbc828d09e10cf988ff8ee9e4ee134
Alviannn <alvianndq@gmail.com>
2023-01-04 16:46:52 +0700

    feat(user): add payload validation and use error response

--- controller/user.go
+++ controller/user.go
@@ -4,34 +4,52 @@ import (
 	"Project_BNCC_GO/config"
 	"Project_BNCC_GO/model"
 	"Project_BNCC_GO/utils"
+	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 func Login(c echo.Context) error {
 	//Login
-	LoginWebData := struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
+	payload := struct {
+		Email    string `json:"email" validate:"required"`
+		Password string `json:"password" validate:"required"`
 	}{}
-	if err := c.Bind(&LoginWebData); err != nil {
-		panic(err)
+
+	if err := c.Bind(&payload); err != nil {
+		return utils.SendResponse(c, utils.BaseResponse{
+			StatusCode: http.StatusBadRequest,
+			Message:    err.Error(),
+		})
 	}
+
 	user := model.User{
-		Email: LoginWebData.Email,
+		Email: payload.Email,
 	}
-	result := db.Where("email = ?", user.Email).Take(&user)
-	if err := result.Error; err != nil {
-		panic(err)
+	if err := db.Where("email = ?", user.Email).Take(&user).Error; err != nil {
+		response := utils.BaseResponse{
+			Message: err.Error(),
+		}
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			response.StatusCode = http.StatusNotFound
+		} else {
+			response.StatusCode = http.StatusInternalServerError
+		}
+
+		return utils.SendResponse(c, response)
 	}
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(LoginWebData.Password)); err != nil {
-		panic(err)
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password)); err != nil {
+		return utils.SendResponse(c, utils.BaseResponse{
+			StatusCode: http.StatusBadRequest,
+			Message:    "Password doesn't match, please try again",
+		})
 	}
 
 	//Generate token
@@ -43,17 +61,20 @@ func Login(c echo.Context) error {
 			ExpiresAt: jwt.NewNumericDate(expTime),
 		},
 	}
-	Algo := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	Token, err := Algo.SignedString(config.JWT_KEY)
+	algo := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := algo.SignedString(config.JWT_KEY)
 	if err != nil {
-		panic(err)
+		return utils.SendResponse(c, utils.BaseResponse{
+			StatusCode: http.StatusBadRequest,
+			Message:    err.Error(),
+		})
 	}
 
 	// Create Cookienya
 	cookie := new(http.Cookie)
 	cookie.Name = "Token"
-	cookie.Value = Token
+	cookie.Value = token
 	cookie.Path = "/"
 	cookie.HttpOnly = true
 
@@ -66,24 +87,36 @@ func Login(c echo.Context) error {
 }
 
 func SignUP(c echo.Context) error {
-	signupUser := model.User{}
+	payload := struct {
+		Username string `json:"username" validate:"required"`
+		Email    string `json:"email" validate:"required"`
+		Password string `json:"password" validate:"required"`
+	}{}
 
-	c.Bind(&signupUser)
+	if err := c.Bind(&payload); err != nil {
+		return utils.SendResponse(c, utils.BaseResponse{
+			StatusCode: http.StatusBadRequest,
+			Message:    err.Error(),
+		})
+	}
 
-	log.Println(signupUser)
-	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(signupUser.Password), bcrypt.DefaultCost)
+	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+	newUser := model.User{
+		Username: payload.Username,
+		Email:    payload.Email,
+		Password: string(hashPassword),
+	}
 
-	signupUser.Password = string(hashPassword)
-
-	result := db.Create(&signupUser)
-
-	if result.Error != nil {
-		panic(result.Error)
+	if err := db.Create(&newUser).Error; err != nil {
+		return utils.SendResponse(c, utils.BaseResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    err.Error(),
+		})
 	}
 
 	return utils.SendResponse(c, utils.BaseResponse{
 		StatusCode: http.StatusCreated,
-		Message:    fmt.Sprintf("Successfully registered a new user with ID %d", signupUser.ID),
+		Message:    fmt.Sprintf("Successfully registered a new user with ID %d", newUser.ID),
 	})
 }
 
